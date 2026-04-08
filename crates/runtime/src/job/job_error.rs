b8ad886e24c9d7bbeb57b253dcbc5ff8da2835d4
Rosnaky <ronakjpatelr22@gmail.com>
2026-04-08 01:18:59 -0400

    Kahn's algorithm for topological sort of DAG

---
+++ crates/runtime/src/job/job_error.rs
@@ -0,0 +1,21 @@
+use crate::task::TaskId;
+
+
+#[derive(Debug, Clone)]
+pub enum JobError {
+    Invalid(String),
+    MissingDependency(TaskId, TaskId),
+    CycleDetected,
+}
+
+impl std::fmt::Display for JobError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            JobError::Invalid(msg) => write!(f, "Job is invalid: {}", msg),
+            JobError::MissingDependency(task_id, missing_dep) => write!(f, "The dependency {} is missing for task {}: ", task_id, missing_dep),
+            JobError::CycleDetected => write!(f, "A cycle was detected"),
+        }
+    }
+}
+
+impl std::error::Error for JobError {}
---
+++ crates/runtime/src/job/mod.rs
@@ -0,0 +1,90 @@
+
+pub mod job_error;
+
+use crate::job::job_error::JobError;
+use crate::task::Task;
+use crate::task::TaskId;
+use std::collections::HashMap;
+use std::collections::VecDeque;
+use std::time::Duration;
+use std::time::Instant;
+
+pub type JobId = String;
+
+pub enum JobState {
+    Pending,
+    Ready,
+    Running { started_at: Instant },
+    Completed { duration: Duration },
+    Failed {
+        error: String,
+        duration: Duration,
+    }
+}
+
+pub struct Job {
+    pub id: JobId,
+    pub tasks: HashMap<TaskId, Task>,
+    pub state: JobState,
+}
+
+impl Job {
+    /// Uses Kahn's algorithm to create topological sort
+    pub fn validate(&self) -> Result<Vec<Vec<TaskId>>, JobError> {
+        
+        let mut queue = VecDeque::new();
+        let mut dep_to_task_id: HashMap<TaskId, Vec<TaskId>> = HashMap::new();
+        let mut task_id_to_indegree: HashMap<TaskId, u64> = HashMap::new();
+
+        for (k, v) in &self.tasks {
+            // Update indegree
+            task_id_to_indegree.insert(k.to_string(), v.spec.depends_on.len() as u64);
+            
+            // Update adj matrix
+            for dep in &v.spec.depends_on {
+                if !self.tasks.contains_key(dep) {
+                    return Err(JobError::MissingDependency(k.clone(), dep.clone()));
+                }
+                dep_to_task_id.entry(dep.clone()).or_insert_with(Vec::new).push(k.clone());
+            }
+        }
+
+
+        for (k, v) in &task_id_to_indegree {
+            if v.clone() == 0 {
+                queue.push_back(k.clone());
+            }
+        }
+
+        let mut ans: Vec<Vec<TaskId>> = Vec::new();
+        while !queue.is_empty() {
+            let mut curr: Vec<TaskId> = Vec::new();
+            let s = queue.len();
+
+            for _ in 0..s {
+                let id = queue.pop_front().unwrap();
+
+                if let Some(deps) = dep_to_task_id.get(&id) {
+                    for dep in deps {
+                        let count = task_id_to_indegree.get_mut(dep).unwrap();
+                        *count -= 1;
+                        if *count == 0 {
+                            queue.push_back(dep.clone());
+                        }
+                    }
+
+                    curr.push(id.clone());
+                }
+
+            }
+            ans.push(curr);
+        }
+
+        let processed: usize = ans.iter().map(|level| level.len()).sum();
+        if processed != self.tasks.len() {
+            return Err(JobError::CycleDetected);
+        }
+
+        Ok(ans)
+    }
+}
