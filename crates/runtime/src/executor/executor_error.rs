f1c252928c1f4526fa3fdc8137b44f36130a8dd7
Rosnaky <ronakjpatelr22@gmail.com>
2026-04-10 22:46:41 -0400

    Docker runtime exector

---
+++ crates/runtime/src/executor/executor_error.rs
@@ -0,0 +1,19 @@
+
+#[derive(Debug, Clone)]
+pub enum ExecutorError {
+    ExecutionFailed(String),
+    ImagePullFailed(String),
+    Timeout,
+}
+
+impl std::fmt::Display for ExecutorError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ExecutorError::ExecutionFailed(msg) => write!(f, "Execution of task failed: {}", msg),
+            ExecutorError::ImagePullFailed(msg) => write!(f, "Container image pulling failed: {}", msg),
+            ExecutorError::Timeout => write!(f, "Execution of task timed out"),
+        }
+    }
+}
+
+impl std::error::Error for ExecutorError {}
